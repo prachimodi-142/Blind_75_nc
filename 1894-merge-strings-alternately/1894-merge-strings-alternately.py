d014e66f283dbdad724adeefb3a686b9201f519d
Prachi Modi <41365106+prachimodi-142@users.noreply.github.com>
2024-09-04 15:18:46 -0700

    Time: 39 ms (27.6%), Space: 16.4 MB (72.19%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+         # find min word size
+         # join values till min
+         # append rest of the word from the longest word
+
+        result = ""
+        total_len = len(word1) + len(word2)
+
+        min_len = min(len(word1), len(word2))
+
+        for i in range(min_len):
+            result += word1[i] + word2[i]
+
+        if len(word1) < len(word2):
+            result += word2[min_len:]
+
+        elif len(word2) < len(word1):
+            result += word1[min_len:]
+
+        return result
+
+        
