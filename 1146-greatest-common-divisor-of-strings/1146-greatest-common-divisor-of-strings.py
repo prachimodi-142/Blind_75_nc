f9e48006a71dac984face31dbff1b958e8a142aa
Prachi Modi <41365106+prachimodi-142@users.noreply.github.com>
2024-09-04 16:02:22 -0700

    Time: 42 ms (23.11%), Space: 16.3 MB (97.69%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,13 @@
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        result = ""
+
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        max_gcd = gcd(len(str1), len(str2))
+
+        return str1[:max_gcd]
